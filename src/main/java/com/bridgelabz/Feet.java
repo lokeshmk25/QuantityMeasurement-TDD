d3ee976fb0d4684300a16eaf630e312784b018dd
lokeshmk25 <lokeshmk2211@gmail.com>
2021-10-29 15:24:02 +0530

    Added: TC 1.1 passed

--- src/main/java/com/bridgelabz/Feet.java
+++ src/main/java/com/bridgelabz/Feet.java
@@ -1,6 +1,24 @@
 package com.bridgelabz;
 
+import java.util.Objects;
+
 public class Feet {
+
+    private final double value;
+
     public Feet(double value) {
+        this.value = value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Feet)) return false;
+        Feet feet = (Feet) o;
+        return Double.compare(feet.value, value) == 0;
+    }
+
 }
+
+
+
--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -7,8 +7,9 @@ public class QuantityTest {
 
     @Test
     public void given0Feetand0FeetSouldReturnEqual() {
-        Feet feet1 = new Feet(0);
-        Feet feet2 = new Feet(0);
-        Assertions.assertEquals(feet1,feet2);
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assertions.assertEquals(feet1, feet2);
     }
+
 }
