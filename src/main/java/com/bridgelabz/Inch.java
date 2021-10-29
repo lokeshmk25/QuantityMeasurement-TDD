35b0b61bea5d012290f604f75e7cecae54df1cba
lokeshmk25 <lokeshmk2211@gmail.com>
2021-10-29 21:30:12 +0530

    Added:TC-1.7 value check for inch

--- src/main/java/com/bridgelabz/Inch.java
+++ src/main/java/com/bridgelabz/Inch.java
@@ -1,9 +1,20 @@
 package com.bridgelabz;
 
+import java.util.Objects;
+
 public class Inch {
     public double value;
 
     public Inch(int value) {
         this.value=value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Inch)) return false;
+        Inch inch = (Inch) o;
+        return Double.compare(inch.value, value) == 0;
+    }
+
 }
--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -16,20 +16,20 @@ public class QuantityTest {
     public void given0Feetand1FeetShouldReturnNotEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(1.0);
-        Assertions.assertNotEquals(feet1,feet2);
+        Assertions.assertNotEquals(feet1, feet2);
     }
 
     @Test
     public void given0FeetandNull_ShouldReturnNotEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = null;
-        Assertions.assertNotEquals(feet1,feet2);
+        Assertions.assertNotEquals(feet1, feet2);
     }
 
     @Test
     void givenFeetandFeet_FromSameRef_WhenEqual_ShouldReturntrue() {
         Feet feet1 = new Feet(0.0);
-        boolean actual=feet1==feet1;
+        boolean actual = feet1 == feet1;
         Assertions.assertTrue(actual);
     }
 
@@ -37,13 +37,23 @@ public class QuantityTest {
     public void givenFeetandFeet_FromDiffType_ShouldReturnNotEqual() {
         Feet feet1 = new Feet(1.2);
         Feet feet2 = new Feet(1);
-        Assertions.assertNotEquals(feet1,feet2);
+        Assertions.assertNotEquals(feet1, feet2);
+    }
+
+
+    @Test
+    void given0inchand0inchShouldReturnEqual() {
+        Inch inch1 = new Inch(0);
+        Inch inch2 = new Inch(0);
+        Assertions.assertEquals(inch1, inch2);
     }
 
     @Test
     void given1Feetcompareto12inch_ShouldReturnTrueWhenEquals() {
         LengtEquality equality = new LengtEquality();
         double comparison = equality.ftinComparison(1);
-        Assertions.assertEquals(12,comparison);
+        Assertions.assertEquals(12, comparison);
     }
 }
+
+
